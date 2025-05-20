880e379278fc9025f74694e810933692465f2a3e
Gabriel Nichio <gabrielnichio000@gmail.com>
2025-05-20 19:26:29 -0300

    feat(agent): agent capable of executing comands on some fixed dataframes
    - dataframes are still fixes

--- agent.py
+++ agent.py
@@ -1,8 +1,12 @@
 import asyncio
 from dotenv import load_dotenv
 
+import pandas as pd
+
+from exec_func import PandasExecutor
 from query_engines import QueryEngine
-from tools import generate_xlsx, load_dataframes
+# from tools import generate_xlsx, load_dataframes
+from exec_func import PandasExecutor
 load_dotenv()
 
 from llama_index.core.agent.workflow import FunctionAgent
@@ -30,24 +34,42 @@ class Agent:
         self._init_agent()
         
     def _set_tools(self):
-        colab = QueryEngine("data/Dados Colaboradores.xlsx")
+        colab = pd.read_excel("data/Dados Colaboradores.xlsx")
+        google = pd.read_excel("data/Ferramentas/Ferramenta 2 - Google workspace.xlsx") 
+
+        pandas_executor = PandasExecutor(colab, google)
+    
+
+        # colab = QueryEngine("data/Dados Colaboradores.xlsx")
         
-        beneficio_unimef = QueryEngine("data/Beneficios/Beneficio 1 - Unimed.xlsx")
+        # beneficio_unimef = QueryEngine("data/Beneficios/Beneficio 1 - Unimed.xlsx")
         
-        colab_qe = QueryEngineTool.from_defaults(
-            colab.get_QE(),
-            name="QueryEngineColaboradores",
-            description="Query engine utilizada para analisar os dados dos colaboradores. Deve ser passado uma query como parametro, que será convertida em um codigo pandas do Python"
+        # colab_qe = QueryEngineTool.from_defaults(
+        #     colab.get_QE(),
+        #     name="QueryEngineColaboradores",
+        #     description="Query engine utilizada para analisar os dados dos colaboradores. Deve ser passado uma query como parametro, que será convertida em um codigo pandas do Python"
+        # )
+        
+        # unimed_qe = QueryEngineTool.from_defaults(
+        #     beneficio_unimef.get_QE(),
+        #     name="QueryEngineBeneficioUnimed",
+        #     description="Query engine que pode ser utilizada para analisar dados do beneficio da unimed dos colaboradores. Deve ser passado uma query como parametro, que será convertida em um codigo pandas do Python."
+        # )
+
+        exec_code = FunctionTool.from_defaults(
+            pandas_executor.execute,
+            name="ExecutaCodigo",
+            description="Funcao capaz de executar uma linha de codigo pandas em dataframes. A funcao recebe o parametro 'codigo', uma string contendo uma linha de codigo pandas. Apenas a biblioteca pandas(pd) esta disponivel no ambiente."
         )
-        
-        unimed_qe = QueryEngineTool.from_defaults(
-            beneficio_unimef.get_QE(),
-            name="QueryEngineBeneficioUnimed",
-            description="Query engine que pode ser utilizada para analisar dados do beneficio da unimed dos colaboradores. Deve ser passado uma query como parametro, que será convertida em um codigo pandas do Python."
+
+        export_xlsx = FunctionTool.from_defaults(
+            pandas_executor.export_df,
+            name="ExportaDataframe",
+            description="Funcao capaz de exportar o dataframe final."
         )
     
         
-        self.tools = [colab_qe, unimed_qe]        
+        self.tools = [exec_code, export_xlsx]        
         
         
     def _init_agent(self):
@@ -55,10 +77,13 @@ class Agent:
             tools=self.tools,
             system_prompt=(
                 """
-                    Você é um agente de IA autônomo que pode utilizar ferramentas para análisar planilhas, ler, padronizar e juntar dados de outras planilhas, e gerar outras planilhas.
-                    As ferramentas utilizadas te possibilitam ler e manipular dados de diferentes fontes utilizando comandos da bilbioteca pandas.
-                    Você possui dados de colaboradores, ferramentas utilizadas pelos colaboradores e benefícios e custos associados a cada colaborador.
-                    Seu trabalho final gerar um .xlsx contendo o ID do colaborador, nome do colaborador, centro de custo, custo por cada ferramenta, custo por cada benefício e custo total.
+                    Você é um agente especializado em análise de dados. Você tem a capacidade de manipular alguns dataframes através de funções. Você consegue manipular um df_final
+                    que está disponível no ambiente de execução. Você possui o dataframe de colaboradores "colab" e o dataframe "google" que você também pode utilizar.
+
+                    Através das suas ferramentas você pode manipular os dados dos dataframes.
+                    A ferramenta ExecutaCodigo pode executar uma linha de codigo pandas com os dataframes mencionados anteriormente. Lá você manipula esses dataframes utilizando apenas a biblioteca pandas(pd).
+
+                    A ferramenta ExportaDataframe é utilizada para exportar o df_final que foi gerado e que esta armazenado no self de uma classe python.
                 """
             )
         )
--- exec_func.py
+++ exec_func.py
@@ -4,8 +4,29 @@ class PandasExecutor:
     def __init__(self, colab, google):
         self.colab = colab
         self.google = google
+        self.df_final: pd.DataFrame
     
     def execute(self, comando):
+        """
+            Funcao que executa apenas uma linha de codigo pandas. Eh possivel realizar operacoes utilizando o dataframe de colaboradores 'colab', o dataframe
+            da ferramenta Google Workspace 'google' e o 'df_final' que eh um: pd.DataFrame({"A": pd.Series(dtype='float')}).
+
+            A funcao recebe:
+                comando: string contendo uma linha de codigo
+
+            O codigo deve ser capaz de ser executado dentro da funcao exec() do Python. Apenas a biblioteca pandas as pd esta disponivel para manipulacoes.
+
+            Essas sao as local_vars:
+
+            local_vars = {
+                "colab": self.colab,
+                "google": self.google,
+                "df_final": pd.DataFrame({"A": pd.Series(dtype='float')})
+            }
+        """
+
+        print("Comando mandado: ", str(comando))
+
         local_vars = {
             "colab": self.colab,
             "google": self.google,
@@ -13,14 +34,23 @@ class PandasExecutor:
         }
         
         exec(comando, globals(), local_vars)
-        
-        self.df = local_vars["df_final"]
-        
-        return self.df
 
-# Uso
-colab = pd.read_excel("data/Dados Colaboradores.xlsx")
-google = pd.read_excel("data/Ferramentas/Ferramenta 2 - Google workspace.xlsx")
-executor = PandasExecutor(colab, google)
-executor.execute("df_final['A'] = colab['Salario'].reset_index(drop=True) + google['Valor Mensal'].reset_index(drop=True)")
-executor.df.to_excel("teste.xlsx", index=False)
+        # print("Local vars: ", local_vars)
+        
+        self.df_final = local_vars["df_final"]
+
+        print("DF_FINAL: ", self.df_final.head())
+
+    def export_df(self):
+        """
+            Funcao capaz de exportar o df_final
+        """
+        self.df_final.to_excel("teste.xlsx")
+
+# # Uso
+# colab = pd.read_excel("data/Dados Colaboradores.xlsx")
+# google = pd.read_excel("data/Ferramentas/Ferramenta 2 - Google workspace.xlsx")
+# executor = PandasExecutor(colab, google)
+# executor.execute("df_final['A'] = colab['Salario'].reset_index(drop=True) + google['Valor Mensal'].reset_index(drop=True)")
+# executor.df.to_excel("teste.xlsx", index=False)
+# some a coluna Salario do dataframe colab e a coluna Valor Mensal do dataframe google, armazene na coluna A do dataframe final e exporte esse dataframe
