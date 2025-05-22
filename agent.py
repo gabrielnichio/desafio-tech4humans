a0d26c94a133e420b295ed5d3efe09ae8d30beef
gabrielnichio <gabrielnichio000@gmail.com>
2025-05-22 14:50:57 -0300

    change for tools with pre-implemented code

--- agent.py
+++ agent.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 import pandas as pd
 
 from exec_func import PandasExecutor
-from query_engines import QueryEngine
-# from tools import generate_xlsx, load_dataframes
 from exec_func import PandasExecutor
 load_dotenv()
 
@@ -24,7 +22,7 @@ import os
 
 class Agent:
     def __init__(self):
-        llm = Anthropic(model="claude-3-5-sonnet-latest", temperature=0, timeout=None, max_retries=2)
+        llm = Anthropic(model="claude-3-5-haiku-latest", temperature=0, timeout=None, max_retries=2)
         # llm = OpenAI(model="gpt-4.1-mini", temperature=0.5, max_retries=2)
         
         Settings.llm = llm
@@ -39,6 +37,8 @@ class Agent:
             if file.endswith(".xlsx"):
                 file_path = os.path.join(data_path, file)
                 self.dataframes[file.replace(".xlsx", "").lower().replace("-", "_").replace(" ", "")] = pd.read_excel(file_path)
+                
+        self.dataframes["df_final"] = pd.DataFrame()
         
         self._set_tools()
         self._init_agent()
@@ -50,23 +50,47 @@ class Agent:
         get_infos = FunctionTool.from_defaults(
             pandas_executor.get_infos,
             name="GetInfos",
-            description="Funcao que retorna as informacoes dos dataframes. Essa funcao deve ser chamada antes de executar qualquer linha de codigo."
+            description="Funcao que retorna as informacoes dos dataframes. Essa funcao deve ser chamada antes de executar qualquer linha de codigo. Ela lista o nome e informações dos dataframes que estão disponíveis para manipulação.",
         )
-
-        exec_code = FunctionTool.from_defaults(
-            pandas_executor.execute,
-            name="ExecutaCodigo",
-            description="Funcao capaz de executar um codigo pandas em dataframes."
+        
+        rename_columns = FunctionTool.from_defaults(
+            pandas_executor.rename_columns,
+            name="RenameColumns",
+            description="Função que renomeia as colunas de um dataframe. Deve ser passado o nome do dataframe, as colunas a serem renomeadas e os novos nomes das colunas. Exemplo: RenameColumns(dataframe='df1', columns=['coluna1', 'coluna2'], new_columns=['nova_coluna1', 'nova_coluna2'])",
         )
-
+        
+        remove_colunas = FunctionTool.from_defaults(
+            pandas_executor.remove_columns,
+            name="RemoveColumns",
+            description="Funcao que remove as colunas de um dataframe. Deve ser passado o nome do dataframe e as colunas a serem removidas. Exemplo: RemoveColumns(dataframe='df1', columns=['coluna1', 'coluna2'])"
+        )
+        
+        criar_dataframe = FunctionTool.from_defaults(
+            pandas_executor.criar_dataframe,
+            name="CriaDataframe",
+            description="Funcao que cria um novo dataframe. Deve ser passado o nome do dataframe e as colunas do dataframe. Exemplo: CriaDataframe(dataframe='df1', columns=['coluna1', 'coluna2'])"
+        )
+        
+        soma_colunas = FunctionTool.from_defaults(
+            pandas_executor.soma_colunas,
+            name="SomaColunas",
+            description="Funcao que soma as colunas de um dataframe. Deve ser passado o nome do dataframe, as colunas a serem somadas e o nome da nova coluna. Exemplo: SomaColunas(dataframe='df1', columns=['coluna1', 'coluna2'], new_column_name='soma')"
+        )
+        
+        merge_dataframes = FunctionTool.from_defaults(
+            pandas_executor.merge_dataframes,
+            name="MergeDataframes",
+            description="Função que faz o merge de dois dataframes. Deve ser passado o nome dos dataframes a serem unidos, a coluna do dataframe da esquerda, coluna do dataframe da direita, o parametro how do merge, e o nome do dataframe de destino. Nunca utilize nomes de dataframes que não existam. Exemplo: MergeDataframes(dataframe1='df1', dataframe2='df2', left_on='coluna_df1', right_on='coluna_df2', how='left', destination='df_final')",
+        )
+        
         export_xlsx = FunctionTool.from_defaults(
             pandas_executor.export_df,
             name="ExportaDataframe",
-            description="Funcao capaz de exportar o dataframe final."
+            description="Funcao capaz de exportar o dataframe final. Deve ser passado um parâmetro de string especificando o nome do dataframe a ser exportado."
         )
     
         
-        self.tools = [exec_code, export_xlsx]        
+        self.tools = [get_infos, rename_columns, remove_colunas, soma_colunas, merge_dataframes, export_xlsx]        
         
         
     def _init_agent(self):
@@ -78,15 +102,41 @@ class Agent:
                                         
                     Através das suas ferramentas você pode ler e manipular os dados dos dataframes.
                     
-                    A ferramenta Executa codigo é utilizada para executar um código python que manipula os dataframes.
-
-                    A ferramenta ExportaDataframe é utilizada para exportar o df_final que foi gerado e que esta armazenado no self de uma classe python.
-                            
-                    Seu trabalho final é exportar um dataframe com as seguintes colunas: ID, Nome, Centro de Custo, Custo por Ferramenta, Custo por Beneficio e Custo Total. Levando em consideração os custos de todas as planilhas que você possui.
+                    Com a ferramenta GetInfos você pode obter informações sobre os dataframes disponíveis para manipulação. Ela deve ser chamada antes de executar qualquer outra função para você saber a estrutura dos dataframes disponíveis. Ela também deve ser executada depois de cada chamada de outra função para você saber o estado do processo. Você pode usar essa ferramenta toda vez que for conveniênte saber os dataframes disponíveis.
                     
+                    A ferramenta RenameColumns é utilizada para renomear as colunas de um dataframe. Você deve passar o nome do dataframe, as colunas a serem renomeadas e os novos nomes das colunas. Utilize ela em todos os dataframes necessários para padronizar o nome das colunas em comum em todos os dataframes. Você pode usar essa ferramenta quantas vezes for necessário para renomear as colunas do dataframe.
+                                   
+                    A ferramenta RemoveColumns é utilizada para remover as colunas de um dataframe. Você deve passar o nome do dataframe e as colunas a serem removidas. Utilize ela em todos os dataframes necessários para remover as colunas que não são necessárias para o seu trabalho. Você pode usar essa ferramenta quantas vezes for necessário para remover as colunas do dataframe.     
+                    
+                    A ferramenta SomaColunas é utilizada para somar as colunas de um dataframe. Você deve passar o nome do dataframe, as colunas a serem somadas e o nome da nova coluna. Ela pode ser utilizada quantas vezes for necessário para somar as colunas do dataframe.
+                                        
+                    A ferramenta MergeDataframes é utilizada para fazer o merge de dois dataframes. Você deve passar o nome dos dataframes a serem unidos, a coluna do dataframe da esquerda, coluna do dataframe da direita, o parametro how do merge e o nome do dataframe de destino. Ela pode ser utilizada quantas vezes for necessário para unir os dataframes.
+                    NUNCA utilize nomes de dataframes que não existem como parâmetro para as funções.
+
+                    A ferramenta ExportaDataframe é utilizada para exportar um dataframe para um arquivo Excel. Você deve passar o nome do dataframe a ser exportado.
+                    
+                    - Considerações importantes:
+                      - Sempre crie um novo dataframe antes de passar um nome novo para as outras funções.
+                      - Utilize apenas os dataframes disponíveis para manipulação.
+                      - Não passe nomes de dataframes que não existem como parâmetro para as funções.
+                      
                     Fluxo de trabalho recomendado:
-                    2. Use ExecutaCodigo() para processar os dados
-                    3. Use ExportaDataframe() para exportar o resultado final
+                    1. Chame a ferramenta GetInfos para obter informações sobre os dataframes disponíveis.
+                    2. Utilize outras ferramentas para manipular os dataframes conforme necessário.
+                    3. Chame a ferramenta GetInfos novamente para verificar o estado do processo.
+                    4. Repita os passos 2 e 3 até que o dataframe final esteja pronto.
+                    5. Exporte o dataframe final utilizando a ferramenta ExportaDataframe.
+                    
+                    Seu trabalho final é exportar um dataframe contendo informações sobre os colaboradores e seus custos.
+                    
+                    O dataframe final DEVE possuir SOMENTE as seguintes colunas: 
+                        -ID: identificador do colaborador (documento do colaborador)
+                        -Nome: Nome do colaborador
+                        -Centro de Custo: centro de custo do colaborador
+                        -Custo por Ferramenta: custo total das FERRAMENTAS utilizadas pelo colaborador
+                        -Custo por Beneficio: custo total dos BENEFICIOS utilizados pelo colaborador
+                        -Custo Total: soma de todos os custos do colaborador (ferramentas + beneficios + salario, + ...))
+                    Levando em consideração os custos de todas as planilhas que você possui.
                 """
             )
         )
@@ -110,14 +160,9 @@ class Agent:
         print(f"Resposta: ${response}")
         return response
     
+    # gere e exporte um dataframe que calcula os custos por colaborador e exporte-o com as colunas: ID, Nome, Centro de Custo, Custo por Ferramenta, Custo por Beneficio e Custo Total
+    
 if __name__ == "__main__":
     agent = Agent()
     
-    while True:
-        print("Digite 'sair' para encerrar o programa.")
-        question = input("Pergunta: ")
-        if question.lower() == "sair":
-            break
-        else:
-            response = asyncio.run(agent.run(question))
-            print(f"Resposta: {response}")
+    asyncio.run(agent.run("gere e exporte um dataframe que calcula os custos por colaborador e exporte-o com as colunas: ID, Nome, Centro de Custo, Custo por Ferramenta, Custo por Beneficio e Custo Total"))
--- exec_func.py
+++ exec_func.py
@@ -1,15 +1,10 @@
 import pandas as pd
 
-from llama_index.llms.anthropic import Anthropic
-
 class PandasExecutor:
     def __init__(self, dataframes):
-        self.llm = Anthropic(model="claude-3-5-sonnet-latest", max_tokens = 2000, temperature=0, timeout=None, max_retries=2)
         
         self.dataframes = dataframes
-        
-        self.df_final = pd.DataFrame({"ID": pd.Series(dtype='str'), "Nome": pd.Series(dtype='str'), "Centro de Custo": pd.Series(dtype='str'), "Custo por Ferramenta": pd.Series(dtype=float), "Custo por Beneficio": pd.Series(dtype=float), "Custo Total": pd.Series(dtype=float)})
-        
+                
     def get_infos(self):
         """
             Funcao que retorna as informacoes dos dataframes
@@ -33,60 +28,84 @@ class PandasExecutor:
                 
         return "\n\n".join(all_info)
     
-    def execute(self, codigo):        
-        f"""
-            Função que capaz de executar um codigo python. As variaveis locais devem ser executadas e um df_final deve ser atribuido. Apenas a biblioteca pandas(pd) pode ser utilizada.            
+    
+    def rename_columns(self, dataframe_to_change: str, columns: list, new_columns: list):
+        """
+            Funcao que renomeia as colunas de um dataframe
+        """
+        new_dataframe: pd.DataFrame = self.dataframes[dataframe_to_change].copy()
+        
+        
+        new_dataframe.rename(columns=dict(zip(columns, new_columns)), inplace=True)
+        
+        self.dataframes[dataframe_to_change] = new_dataframe
+        
+        print(f"Colunas {columns} renomeadas para {new_columns} com sucesso!\n\n")
+        
+        
+        
+    def remove_columns(self, dataframe_to_change: str, columns: list):
+        """
+            Funcao que remove as colunas de um dataframe
+        """
+        new_dataframe: pd.DataFrame = self.dataframes[dataframe_to_change].copy()
+        
+        new_dataframe.drop(columns=columns, inplace=True)
+        
+        self.dataframes[dataframe_to_change] = new_dataframe
+        
+        print(f"Colunas {columns} removidas com sucesso!\n\n")
+        
+        
+        
+        
+    def criar_dataframe(self, dataframe_name: str, columns: list):
+        """
+            Funcao que cria um novo dataframe
+        """
+        new_dataframe = pd.DataFrame(columns=columns)
+                
+        self.dataframes[dataframe_name] = new_dataframe
+        
+        print(f"Dataframe {dataframe_name} criado com sucesso!\n\n")
+        
+        
+        
+    def soma_colunas(self, dataframe: str, columns: list, new_column_name: str):
+        """
+            Funcao que soma as colunas de um dataframe
         """
         
-        response = self.llm.complete(
-            prompt=f"""
-                Forneça APENAS código Python sem explicações adicionais.
-                O código deve ser conciso, funcional e pronto para execução.
-                Não inclua blocos de markdown, símbolos de cópia, linhas de comentário iniciais como ```python ou ``` no final.
-                Seu código será executado diretamente com exec() em Python.
-                Você deve usar a biblioteca pandas (pd) para manipular os dataframes. 
-                
-                O seu código deve ser capaz de manipular os dataframes:
-                
-                {self.get_infos()}
-                
-                e construir um dataframe final (df_final) com as seguintes colunas:
-                ID, Nome, Centro de Custo, Custo por Ferramenta, Custo por Beneficio e Custo Total.
-                
-                Você está trabalhando em um ambiente local. As variaveis dos dataframes que você usar no código serão as variaveis locais.
-            """
-        )
+        df = self.dataframes[dataframe].copy()
         
-        code = response.text.strip()
-    
-        # Remover blocos de código markdown se existirem
-        if code.startswith("```python"):
-            code = code.replace("```python", "", 1)
-        if code.endswith("```"):
-            code = code[:-3]
-            
-        print("\n\n--------------\n\nCodigo: ", code.strip(), "\n\n")
+        df[new_column_name] = df[columns].sum(axis=1)
+        
+        self.dataframes[dataframe] = df
+                
+        
+        print(f"Colunas {columns} somadas com sucesso e armazenadas na coluna {new_column_name}!\n\n")
+    def merge_dataframes(self, dataframe1: str, dataframe2: str, left_on: str, right_on: str, how: str, destination: str):
+        """
+            Funcao que faz o merge de dois dataframes
+        """
+        
+        df1 = self.dataframes[dataframe1].copy()
+        df2 = self.dataframes[dataframe2].copy()
+        
+        new_dataframe = pd.merge(df1, df2, left_on=left_on, right_on=right_on, how=how)
+        
+        self.dataframes[destination] = new_dataframe
+        
+        print(f"Dataframes {dataframe1} e {dataframe2} unidos com sucesso!\n\n")
+        
 
-        local_vars = {}
-        
-        for dataframe in self.dataframes:
-            local_vars[dataframe] = self.dataframes[dataframe]
-        
-        local_vars["df_final"] = self.df_final
-                    
-        exec(code.strip(), globals(), local_vars)
-        
-        self.df_final = local_vars["df_final"]
-
-        print("\n\n--------------\n\nDF_FINAL: ", self.df_final.head(), "\n\n")
-
-    def export_df(self):
+    def export_df(self, dataframe_to_export: str):
         """
             Funcao capaz de exportar o df_final
         """
-        self.df_final.to_excel("teste.xlsx")
+        self.dataframes[dataframe_to_export][["ID", "Nome", "Centro de Custo", "Custo por Ferramenta", "Custo por Beneficio", "Custo Total"]].to_excel("teste.xlsx")
         
-        print("Dataframe exportado com sucesso!")
+        print("Dataframe exportado com sucesso!\n\n")
 
 
 # gere o dataframe final calculando os custos para cada colaborador. Manipule e padronize os dados das diferentes planilhas para conseguir fazer isso corretamente
